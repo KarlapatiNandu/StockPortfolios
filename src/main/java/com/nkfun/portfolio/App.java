ef78b292cc0d0feee6eabd3f7b80a63e5724cd29
KN-1-dev <nandu211020@gmail.com>
2025-09-13 22:33:57 +0530

    Updated app.java to work with the input of tickerSymbols

--- src/main/java/com/nkfun/portfolio/App.java
+++ src/main/java/com/nkfun/portfolio/App.java
@@ -11,38 +11,26 @@ public class App
         // Creating the services needed for api fetch:
         MarketDataService marketDataService = new AlphaVantageService();
         PortfolioStorageService storageService = new PortfolioStorageService();
-
+        
+        // loading the portfolio from the file is already present, if not then creating a new portfolio
         Portfolio myPortfolio = storageService.loadPortfolio(marketDataService);
-        myPortfolio.setMarketDataService(marketDataService);
+        myPortfolio.setMarketDataService(marketDataService); // set a new marketService
+
 
         System.out.println("Current state of the portfolio: ");
         System.out.println(myPortfolio);
+        storageService.savePortfolio(myPortfolio); // saving initial state
+        System.out.println("---------------*************---------------");
+
+
+        AnalysisEngine analysisEngine = new AnalysisEngine();
+        analysisEngine.runAnalysis("AAPL");
+
 
         // Creating hardcoded Stocks for testing purposes:
         // (tickerSymbol, companyName, quantity, purchasePrice)
-        Stock appleStock = new Stock("AAPL", "Apple Inc", 20, 1.65);
-        Stock googleStock = new Stock("GOOGL", "Alphabet Inc", 50, 1.80);
-        Stock teslaStock = new Stock("TSLA", "Tesla Inc", 100, 1.55);
+        // purchase price implying at what price the person bought the shares, whereas the total value of the portfolio is the current amount 
+        // to be earned by the portfolio if he sells all his shares 
         
-
-        // appending the stocks to myPortfolio:
-        myPortfolio.addStock(appleStock);
-        myPortfolio.addStock(googleStock);
-        myPortfolio.addStock(teslaStock);
-
-        Stock applStock = new Stock("AAPL", "Apple Inc", 50, 90);
-        myPortfolio.addStock(applStock);
-        // Saving the portfolio onto file:
-        storageService.savePortfolio(myPortfolio);
-
-        // Displaying the stocks in myPortfolio:
-        // System.out.println("\n Final Portfolio State: ");
-        // System.out.println(myPortfolio); // here the program will directly call toString which was overriden
-        System.out.println("\nCalculating portfolio's total value: ");
-        double totalValue = myPortfolio.calculateTotalValue();
-        
-        System.out.println("---------*******-----------");
-        System.out.printf("Total Portfolio value is : $%.2f\n",totalValue);
-        System.out.println("---------*******-----------");
     }
 }
