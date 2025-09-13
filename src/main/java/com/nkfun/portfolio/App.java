361ff5e50bdca56e0d1651174489771bd9f13482
KN-1-dev <nandu211020@gmail.com>
2025-09-13 12:03:43 +0530

    Added the creation of services, file object, saving the stocks to the file, loading portfolios total value from the file

--- src/main/java/com/nkfun/portfolio/App.java
+++ src/main/java/com/nkfun/portfolio/App.java
@@ -7,23 +7,33 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("\n--------------Portfolio Management System-------------------");
-        MarketDataService marketDataService = new AlphaVantageService() {
-            
-        };
-        // Creating a portfolio:
-        Portfolio myPortfolio = new Portfolio(marketDataService);
+        
+        // Creating the services needed for api fetch:
+        MarketDataService marketDataService = new AlphaVantageService();
+        PortfolioStorageService storageService = new PortfolioStorageService();
+
+        Portfolio myPortfolio = storageService.loadPortfolio(marketDataService);
+        myPortfolio.setMarketDataService(marketDataService);
+
+        System.out.println("Current state of the portfolio: ");
+        System.out.println(myPortfolio);
 
         // Creating hardcoded Stocks for testing purposes:
         // (tickerSymbol, companyName, quantity, purchasePrice)
-        Stock appleStock = new Stock("AAPL", "Apple Inc", 20, 150.65);
-        Stock googleStock = new Stock("GOOGL", "Alphabet Inc", 50, 2750.80);
-        Stock teslaStock = new Stock("TSLA", "Tesla Inc", 100, 700.55);
+        Stock appleStock = new Stock("AAPL", "Apple Inc", 20, 1.65);
+        Stock googleStock = new Stock("GOOGL", "Alphabet Inc", 50, 1.80);
+        Stock teslaStock = new Stock("TSLA", "Tesla Inc", 100, 1.55);
         
 
         // appending the stocks to myPortfolio:
         myPortfolio.addStock(appleStock);
         myPortfolio.addStock(googleStock);
         myPortfolio.addStock(teslaStock);
+
+        Stock applStock = new Stock("AAPL", "Apple Inc", 50, 90);
+        myPortfolio.addStock(applStock);
+        // Saving the portfolio onto file:
+        storageService.savePortfolio(myPortfolio);
 
         // Displaying the stocks in myPortfolio:
         // System.out.println("\n Final Portfolio State: ");
